19b8b689d9c85fa186398410879bbac6837ddb9a
iMubashirSyed <mubashir.edu@outlook.com>
2025-12-28 15:46:18 +0500

    Backend Authorization done

--- Backend/dyslexia_app_backend/dyslexia_app_backend/settings.py
+++ Backend/dyslexia_app_backend/dyslexia_app_backend/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
     'umair',
     'mubashir',
     'mateen',
@@ -118,3 +119,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.SessionAuthentication',
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+
+    ]
+}
--- Backend/dyslexia_app_backend/mubashir/views.py
+++ Backend/dyslexia_app_backend/mubashir/views.py
@@ -1,3 +1,33 @@
 from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status, permissions, authentication
+from .serializers import RegisterSerializer
+from rest_framework_simplejwt.tokens import RefreshToken
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate
+
 
 # Create your views here.
+class RegisterView(APIView):
+    def post(self, request):
+        data = request.data
+        serializer = RegisterSerializer(data=data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response({"message": "User registered successfully.", 
+                             "data": data}, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    
+    
+class LoginView(APIView):
+    def post(self, request):
+        data = request.data
+        username = data.get('username')
+        password = data.get('password')
+        user = authenticate(username=username, password=password)
+        if user:
+            refresh = RefreshToken.for_user(user)
+            return Response({"message": "User logged in successfully.", 
+                             "RefreshToken": str(refresh), "AceessToken": str(refresh.access_token)}, status=status.HTTP_200_OK)
+        return Response({"message": "Invalid credentials."}, status=status.HTTP_401_UNAUTHORIZED)
